af6a05126e717363eb74fdc8e440f148b90389e9
Patryk Prusinowski <patryk.prusinowsky@gmail.com>
2020-10-29 02:31:19 +0100

    First website

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Running at http://localhost:${port}`)
+})
